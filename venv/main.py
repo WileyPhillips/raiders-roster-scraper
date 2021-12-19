f0e3783aeb5013e10341b13adf6c64e9bc1bd218
Wiley <wileystphillips@gmail.com>
2021-12-19 03:43:42 -0500

    Switch sites for future team use

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.raiders.com/team/players-roster/"
+URL = "https://www.nfl.com/teams/las-vegas-raiders/roster"
 
 page = requests.get(URL)
 
@@ -12,9 +12,7 @@ player_elements = results.find_all("tr")
 i=0
 j=0
 for player_element in player_elements:
-    player = player_element.find("td", class_="sorter-custom-height")
-    if player == None:
-        j += 1
+    player = player_element.find("a", class_="nfl-o-roster__player-name nfl-o-cta--link")
     print(player, end="\n"*2)
     i +=1
 print(i)
